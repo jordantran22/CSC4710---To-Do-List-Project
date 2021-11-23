0610a3de3b8e1e3576707b0864799a3cf9bdc970
jordantran22 <jordan22tran@gmail.com>
2021-11-23 16:04:24 -0500

    Changed styles, improved UI

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,15 +3,29 @@ import React from 'react'
 const Task = ({task}) => {
     return (
         <div className="taskComponent">
-            <div>
+            <div className="taskAttributes">
                 <div>Task: </div>
                 <div>{task.text}</div>
             </div>
 
-            <div>
+            <div className="taskAttributes">
+                <div>Category: </div>
+
+            </div>
+
+
+            <div className="taskAttributes">
                 <div>Due Date: </div>
                 <div>{task.date}</div>
-                
+            </div>
+
+            <div className="taskAttributes">
+                <div>Status: </div>
+            </div>
+
+            <div className="taskAttributes">
+                <div>Priority: </div>
+                <div>{task.priorityLevel}</div>
             </div>
         </div>
     )
--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -6,6 +6,10 @@ const ToDoContainer = () => {
     const [taskDescription, setTaskDescription] = useState('');
     const [tasks, setTasks] = useState([]);
     const [date, setDate] = useState([]);
+    const [newCategory, setNewCategory] = useState('');
+    const [taskCategory, setTaskCategory] = useState('');
+    const [priorityLevel, setPriorityLevel] = useState(0);
+    const [categories, setCategories] = useState([]); // onEffect -> make api request for categories
 
     const onSubmit = (e) => {
         e.preventDefault();
@@ -13,6 +17,8 @@ const ToDoContainer = () => {
         const newTask = {
             id:  Math.floor(Math.random() * 10000) + 1,
             text: taskDescription,
+            newCategory: newCategory,
+            priorityLevel: priorityLevel,
             date: date
         }
         addNewTask(newTask);
@@ -26,9 +32,37 @@ const ToDoContainer = () => {
         <div className="toDoContainer">
             <h1> To Do List! </h1>
             <form className="form" onSubmit={onSubmit}>
-                <input type="text" placeholder="Enter your task" value={taskDescription} onChange={((e) => setTaskDescription(e.target.value))}></input>
+                    <div>Task Description: </div>
+                     <input type="text" placeholder="Enter your task" value={taskDescription} onChange={((e) => setTaskDescription(e.target.value))}></input>
+               
+
+                <div>Create New Category: </div>
+                <input type="text" placeholder="New Category" value={newCategory} onChange={((e) => setNewCategory(e.target.value))}></input>
+
+                <div>Select Existing Category: </div>
+                <select className="" value={taskCategory} onChange={((e) => setTaskCategory(e.target.value))}>
+                <option value="" selected disabled hidden>Choose here</option>
+                    {Object.entries(categories).map(([key, value]) => {
+                        return (
+                        <option className="" value={key}>
+                            {value}
+                        </option>
+                        );
+                    })}
+                </select>
+
+                <div>Choose Priority Level: </div>
+                <select className="" value={priorityLevel} onChange={((e) => setPriorityLevel(e.target.value))}>
+                    <option value="" selected disabled hidden>Choose here</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                </select>
+
+                <div>Pick A Due Date: </div>
                 <input type="date" value={date} onChange={((e) => setDate(e.target.value))}/>
-                <input type="submit"></input>
+                <button className="addTask">Add Task</button>
             </form>
 
            
