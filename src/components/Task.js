8ce3ced9b82c20e79b1bf57c2ca35b82bc155d1e
jordantran22 <jordan22tran@gmail.com>
2021-11-23 15:36:01 -0500

    Implemented user input for date

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Task = ({task}) => {
     return (
-        <div>
-            <div>{task.text}</div>
+        <div className="taskComponent">
+            <div>
+                <div>Task: </div>
+                <div>{task.text}</div>
+            </div>
+
+            <div>
+                <div>Due Date: </div>
+                <div>{task.date}</div>
+                
+            </div>
         </div>
     )
 }
--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -5,12 +5,15 @@ import Task from './Task.js';
 const ToDoContainer = () => {
     const [taskDescription, setTaskDescription] = useState('');
     const [tasks, setTasks] = useState([]);
+    const [date, setDate] = useState([]);
 
     const onSubmit = (e) => {
         e.preventDefault();
+   
         const newTask = {
             id:  Math.floor(Math.random() * 10000) + 1,
-            text: taskDescription
+            text: taskDescription,
+            date: date
         }
         addNewTask(newTask);
     }
@@ -24,7 +27,7 @@ const ToDoContainer = () => {
             <h1> To Do List! </h1>
             <form className="form" onSubmit={onSubmit}>
                 <input type="text" placeholder="Enter your task" value={taskDescription} onChange={((e) => setTaskDescription(e.target.value))}></input>
-                <input type="date" />
+                <input type="date" value={date} onChange={((e) => setDate(e.target.value))}/>
                 <input type="submit"></input>
             </form>
 
