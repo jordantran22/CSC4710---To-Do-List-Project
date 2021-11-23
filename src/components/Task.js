89259fbb90343eaa23dcf90b2866e77162c20959
Jordan Tran <jordan22tran@gmail.com>
2021-11-23 11:30:03 -0500

    Implemented adding task functionality

---
+++ src/components/Task.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Task = ({task}) => {
+    return (
+        <div>
+            <div>{task.text}</div>
+        </div>
+    )
+}
+
+export default Task
---
+++ src/components/ToDoContainer.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {useEffect, useState} from 'react';
+import Task from './Task.js';
+
+const ToDoContainer = () => {
+    const [taskDescription, setTaskDescription] = useState('');
+    const [tasks, setTasks] = useState([]);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const newTask = {
+            id:  Math.floor(Math.random() * 10000) + 1,
+            text: taskDescription
+        }
+        addNewTask(newTask);
+    }
+    const addNewTask = (newTask) => {
+        setTasks([...tasks, newTask]);
+        console.log(tasks);
+    }
+
+    return (
+        <div className="toDoContainer">
+            <h1> To Do List! </h1>
+            <form className="form" onSubmit={onSubmit}>
+                <input type="text" placeholder="Enter your task" value={taskDescription} onChange={((e) => setTaskDescription(e.target.value))}></input>
+                <input type="date" />
+                <input type="submit"></input>
+            </form>
+
+           
+            <div>
+                {tasks.map((task) => (
+                <Task task={task}></Task>
+                ))}
+            </div>
+          
+
+        </div>
+    )
+}
+
+export default ToDoContainer
